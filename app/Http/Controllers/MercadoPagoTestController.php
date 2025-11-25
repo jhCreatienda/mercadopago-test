5e3a36cddda8fafa9a0cd55a0a07da3cff1ece7c
jhernandez <jhcreatienda@gmail.com>
2025-11-25 12:00:31 -0500

    create project

---
+++ app/Http/Controllers/MercadoPagoTestController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MercadoPagoTestController extends Controller
+{
+    //
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MercadoPagoTestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/mercadopago/test', [MercadoPagoTestController::class, 'index']);
+Route::post('/mercadopago/create-preference', [MercadoPagoTestController::class, 'createPreference']);
+Route::get('/mercadopago/success', [MercadoPagoTestController::class, 'success']);
+Route::get('/mercadopago/failure', [MercadoPagoTestController::class, 'failure']);
+Route::get('/mercadopago/pending', [MercadoPagoTestController::class, 'pending']);
+Route::post('/mercadopago/webhook', [MercadoPagoTestController::class, 'webhook']);
+Route::get('/mercadopago/payment/{id}', [MercadoPagoTestController::class, 'getPayment']);
